93222fe8929715cd3f431a8861b846879d8cc420
Brian <brianflaherty64@gmail.com>
2016-02-19 17:03:51 -0500

    mocked up basic interface

---
+++ www/witai.js
@@ -0,0 +1,26 @@
+// Examples:
+// https://github.com/apache/cordova-plugin-media-capture/blob/master/www/capture.js
+// https://github.com/apache/cordova-plugin-camera/blob/master/www/Camera.js
+
+// var exec = require('cordova/exec');
+
+//
+// Creates UI interface for Wit.ai.
+//
+// @param {Function} successCB
+// @param {Function} errorCB
+ 
+
+// WitaiClient = {}
+
+// WitaiClient.init = function(successCallback, errorCallback, options) {
+  // exec(successCallback, errorCallback, 'Witai', 'init', [options])
+// }
+
+// WitaiClient.render = function(successCallback, errorCallback, options) {
+  // exec(successCallback, errorCallback, 'Witai', 'render')
+// }
+
+// 
+
+// module.exports = (token) -> new WitaiClient(token);
