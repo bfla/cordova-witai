48266db4388812d301ef440fd89ff5ca4255f5dc
Brian <brianflaherty64@gmail.com>
2016-02-21 12:06:39 -0500

    updated args for cordova interface

--- src/ios/CDVWitai.m
+++ src/ios/CDVWitai.m
@@ -17,13 +17,26 @@
 
 - (void)initMicrophone:(CDVInvokedUrlCommand*)command
 {
+    CDVPluginResult.keepCallback = true; // Let callback fire multiple times
+    CDVPluginResult* pluginResult = nil;
+    NSString* token = [command.arguments objectAtIndex:0];
+
+    if (token == nil) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg[0] (wit.ai token) was null"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    } else {
+        
+    }
+
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
     [[AVAudioSession sharedInstance] setActive:YES error:nil];
     
     // FIXIT -- function must take an access token
-    [Wit sharedInstance].accessToken = @"YOUR_ACCESS_TOKEN"; // replace xxx by your Wit.AI access token
+    [Wit sharedInstance].accessToken = token; // replace xxx by your Wit.AI access token
     //enabling detectSpeechStop will automatically stop listening the microphone when the user stop talking
     [Wit sharedInstance].detectSpeechStop = WITVadConfigDetectSpeechStop;
+
+    // this.witCallback = ;
 
 }
 
